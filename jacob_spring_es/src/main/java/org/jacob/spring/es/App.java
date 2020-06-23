6c2798b3ee0ccd123c0605ec4c67715de9f0b339
Jacob <jacob1107@163.com>
2020-06-23 19:24:18 +0800

    支持log4j 日志模式

---
+++ jacob_spring_es/src/main/java/org/jacob/spring/es/App.java
@@ -0,0 +1,13 @@
+package org.jacob.spring.es;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
