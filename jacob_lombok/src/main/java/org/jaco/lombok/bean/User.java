9d6af03a9afb4aba03f1b5e5c903e4398a11f18b
Jacob <jacob1107@163.com>
2021-03-18 18:23:47 +0800

    lombok 注解提交

--- jacob_lombok/src/main/java/org/jaco/lombok/bean/User.java
+++ jacob_lombok/src/main/java/org/jaco/lombok/bean/User.java
@@ -1,23 +1,25 @@
 package org.jaco.lombok.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+//@EqualsAndHashCode(callSuper = true)
 public class User {
 	
 	private String name;
 	private String sex;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getSex() {
-		return sex;
-	}
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
+	/*
+	 * public String getName() { return name; } public void setName(String name) {
+	 * this.name = name; } public String getSex() { return sex; } public void
+	 * setSex(String sex) { this.sex = sex; }
+	 */
 
 }
