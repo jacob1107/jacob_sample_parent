3203c39ea77b8d585c60f5bff029d3a484c6623f
Jacob <jacob1107@163.com>
2021-04-18 21:51:20 +0800

    sentinel 集群模式

--- jacob_spring_sentinel/src/main/java/org/jacob/spring/sentinel/parser/ServerRequestOriginParser.java
+++ jacob_spring_sentinel/src/main/java/org/jacob/spring/sentinel/parser/ServerRequestOriginParser.java
@@ -5,7 +5,7 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 import com.alibaba.csp.sentinel.adapter.servlet.callback.RequestOriginParser;
 
-@Component
+//@Component
 public class ServerRequestOriginParser implements RequestOriginParser {
 	/**
 	 * 区分来源：本质通过request域获取来源标识 根据不同来源如app/pc，最后返回结果值交给sentinel流控匹配处理
@@ -14,9 +14,11 @@ public class ServerRequestOriginParser implements RequestOriginParser {
 	public String parseOrigin(HttpServletRequest request) {
 		String servName = request.getParameter("servName");
 		// 这个不是授权规则必须的判断
+
 		if (StringUtils.isEmpty(servName)) {
 			throw new RuntimeException("servName不能为空");
 		}
+
 		return servName;
 	}
 }
