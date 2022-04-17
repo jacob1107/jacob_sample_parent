e7a02f8ded5c64c79e50974fed3650d9d72fb689
Jacob <jacob1107@163.com>
2022-04-17 12:59:32 +0800

    es 相关过时方法更新

---
+++ jacob_rocketmq/src/main/java/App.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		/*
+		 * System.setProperty("KUBERNETES_SERVICE_HOST", "10.96.0.1");
+		 * System.setProperty("KUBERNETES_SERVICE_PORT", "443");
+		 */
+		System.setProperty("SERVICE_HOST", "10.96.0.1");
+		System.setProperty("SERVICE_PORT", "443");
+		SpringApplication.run(App.class, args);
+	}
+}
