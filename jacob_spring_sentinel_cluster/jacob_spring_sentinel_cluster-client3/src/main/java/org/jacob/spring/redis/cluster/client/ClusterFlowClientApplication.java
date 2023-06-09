23d29fd234d09faa65c49c14b559a3cd15ae4344
Jacob <jacob1107@163.com>
2023-06-10 07:54:38 +0800

    tomcat

---
+++ jacob_spring_sentinel_cluster/jacob_spring_sentinel_cluster-client3/src/main/java/org/jacob/spring/redis/cluster/client/ClusterFlowClientApplication.java
@@ -0,0 +1,23 @@
+package org.jacob.spring.redis.cluster.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动时加参数： -Dproject.name=xxx -Dcsp.sentinel.dashboard.server=consoleIp:port
+ * 可以在对应的 sentinel 的 dashboard 中查看效果
+ * 
+ * @author houyi
+ * @date 2019-01-19
+ */
+@SpringBootApplication
+public class ClusterFlowClientApplication {
+	public static void main(String[] args) {
+
+		// System.setProperty("project.name", "sentinel-clinet");
+		System.setProperty("project.name", "sentinel");
+		System.setProperty("csp.sentinel.log.use.pid", "true");
+		System.setProperty("csp.sentinel.dashboard.server", "127.0.0.1:8089");
+		SpringApplication.run(ClusterFlowClientApplication.class, args);
+	}
+}
